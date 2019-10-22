c4c033f48e8bd0aae0b9685c87d60cfbcbf2720b
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2019-10-23 03:56:00 +0530

    logisticReg added

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 20 13:54:24 2019
+Created on Wed Oct 23 03:12:26 2019
 
 @author: SRIJAN
 """
@@ -58,24 +58,23 @@ df['processed_text'] = df.tweet.apply(lambda row : preprocess_text(row))
 df.head()
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 
-tfidf_vec = TfidfVectorizer(ngram_range=(1,3))
+tfidf_vec = TfidfVectorizer(ngram_range=(1,2))
 tfidf_data = tfidf_vec.fit_transform(df.processed_text)
 #tfidf_data = pd.DataFrame(tfidf_data).toarray()
 #tfidf_data.head()
 
 X_train, X_test, y_train, y_test = train_test_split(tfidf_data, df['label'], test_size=0.0, random_state = 42)
 
-spam_filter = MultinomialNB(alpha=0.2)
-spam_filter.fit(X_train, y_train)
-
+# Logistic regression
+from sklearn.linear_model import LogisticRegression
+clf_log = LogisticRegression(random_state=2019, C=10000).fit(X_train, y_train)
 
 
 # ------------------------Testing-------------------------------
 
-predictions = spam_filter.predict(X_test).tolist()
+predictions = clf_log.predict(X_test).tolist()
 wrong = []
 count = 0
 for i in range(len(y_test)):
@@ -91,26 +90,15 @@ from sklearn.metrics import classification_report
 print(classification_report(predictions, y_test))
 
 # ---------------------------------------------------------------
-
-text = "yoyas market deming nm sell beer white said closed hispanic walked n sold"
-
-
 df_test = pd.read_csv('dataset/test_tweets_anuFYb8.csv', encoding='latin-1')
 df_test.head()
 
 df_test['processed_text'] = df_test.tweet.apply(lambda row : preprocess_text(row))
 df_test.head()
 
-"""
-for row in df_test.processed_text:
-    #print(type(row))
-    t = tfidf_vec.transform(row)
-    spam_filter.predict(t)[0]
-"""
-    
 t = tfidf_vec.transform(df_test.processed_text)
 
-df_test["prediction"] = spam_filter.predict(t)
+df_test["prediction"] = clf_log.predict(t)
 
 submission = pd.read_csv('submission.csv', encoding='latin-1')
 submission['id'] = df_test['id']
@@ -118,12 +106,7 @@ submission['label'] = df_test['prediction']
 
 df_test['prediction'].value_counts()
 
-pd.DataFrame(submission, columns=['id','label']).to_csv('submission1.csv', index = False)
-
-
-
-
-
+pd.DataFrame(submission, columns=['id','label']).to_csv('submission3.csv', index = False)
 
 
 
